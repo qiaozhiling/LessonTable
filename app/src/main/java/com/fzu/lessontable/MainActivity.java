65386d8dfbe3facfc38266a3468223bf624a0424
qiaozhiling <1890509733@163.com>
2020-12-21 00:09:07 +0800

    initial project02

--- app/src/main/java/com/fzu/lessontable/MainActivity.java
+++ app/src/main/java/com/fzu/lessontable/MainActivity.java
@@ -4,11 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        try {
+            HttpURLConnection connection;
+            URL url = new URL("htp");
+            connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("post");
+        }catch (Exception e){
+
+        }
     }
 }
