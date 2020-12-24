acd3f036d6cccb90a6ae44cfa6eaf43ca6445a63
qiaozhiling <1890509733@163.com>
2020-12-24 22:58:04 +0800

    initial project04

--- app/src/main/java/com/fzu/lessontable/MainActivity.java
+++ app/src/main/java/com/fzu/lessontable/MainActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.jacky.lession.LessionDetail;
+import com.jacky.lession.LessionTable;
+
 import java.net.HttpURLConnection;
 import java.net.URL;
 
@@ -13,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        LessionTable table=new LessionTable("adad","sdd");
         //试试能不能push上
     }
 }
