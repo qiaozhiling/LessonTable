eb66382cdbf5f55165db35abfe04fa707f4f101a
qiaozhiling <1890509733@163.com>
2020-12-26 00:38:43 +0800

    simple UI 01

--- app/src/main/java/com/fzu/lessontable/MainActivity.java
+++ app/src/main/java/com/fzu/lessontable/MainActivity.java
@@ -1,14 +1,8 @@
 package com.fzu.lessontable;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-import com.jacky.lession.LessionDetail;
-import com.jacky.lession.LessionTable;
-
-import java.net.HttpURLConnection;
-import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,7 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        LessionTable table=new LessionTable("adad","sdd");
         //试试能不能push上
+
+
     }
 }
